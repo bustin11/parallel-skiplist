cc44cde24a5ee5a3ab75095a324d1c954361bee9
Justin Hsu <justinhsu@pop-os.localdomain>
2022-06-04 13:39:49 -0700

    added more performance metrics

--- run.sh
+++ run.sh
@@ -6,16 +6,23 @@ set -o xtrace   # print cmds
 set -e          # errors
 
 make clean
-make "test"
 
-echo $'\n===== Testing for correctness... =====\n'
-${PATH_TO}/tests/test -N 4
+if [ "$1" != "main" ]
+then
 
-make
-echo $'\n===== Benchmarking performance of skiplist... =====\n'
+    make "test"
+    echo $'\n===== Testing for correctness... =====\n'
+    ${PATH_TO}/tests/test -N 4
 
-${PATH_TO}/src/main -N 1
+fi
 
-${PATH_TO}/src/main -N 2
+if [ "$1" != "test" ]
+then
 
-${PATH_TO}/src/main -N 4
+    make
+    echo $'\n===== Benchmarking performance of skiplist... =====\n'
+    ${PATH_TO}/src/main -N 1
+    ${PATH_TO}/src/main -N 2
+    ${PATH_TO}/src/main -N 4
+
+fi
